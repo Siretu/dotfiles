479d5e07c9542e6f90f0cfab1117718e4695a1fa
Erik Ihrén <erikihr@gmail.com>
2015-07-11 22:08:29 +0200

    Added setup file

---
+++ setup.sh
@@ -0,0 +1,14 @@
+SYMLINK_LIST="
+.bashrc
+.bash_aliases
+"
+
+BASE_DIR=$(cd $(dirname $0);pwd)
+cd $BASE_DIR
+
+for FILE in $SYMLINK_LIST; do
+    rm -rf ~/$FILE
+    ln -s $BASE_DIR/$FILE ~/$FILE
+done
+
+echo Complete
